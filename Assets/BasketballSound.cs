57d04a86d2ebd0666f8fe27931a77390edc55a43
Luke Chadwick <me@vertis.io>
2018-04-16 12:04:50 +1000

    prototype scoring

---
+++ Assets/BasketballSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketballSound : MonoBehaviour {
+    public AudioClip clip;    // Add your Audio Clip Here;
+                             // This Will Configure the  AudioSource Component; 
+                             // MAke Sure You added AudioSouce to death Zone;
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = clip;
+    }
+
+    void OnCollisionEnter()  //Plays Sound Whenever collision detected
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ Assets/Goal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour {
+
+    public Text scoreText;
+    int score = 0;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello " + gameObject.name);
+        
+	}
+
+    //void OnCollisionEnter(Collision col)
+    //{
+    //    Debug.Log("Collision " + gameObject.name);
+    //    Destroy(col.contacts[0].otherCollider.gameObject);
+    //}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        score++;
+        //Debug.Log("Goal " + score);
+        scoreText.text = score.ToString();
+    }
+}
