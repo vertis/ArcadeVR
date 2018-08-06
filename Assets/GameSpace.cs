5bd22305ab76bc698b63d9e610a57b4e4e60a310
Luke Chadwick <me@vertis.io>
2018-08-06 20:49:14 +1000

    Final changes

---
+++ Assets/GameSpace.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSpace : MonoBehaviour {
+
+    public Transform spawner;
+    public GameObject templateObject;
+
+    void OnTriggerExit(Collider other)
+    {
+        // Destroy everything that leaves the trigger
+        //Destroy(other.gameObject);
+        Debug.Log(spawner.transform.position);
+        //other.gameObject.transform.position = spawner.transform.position;
+        //other.gameObject.rigidbody.
+        Destroy(other.gameObject);
+        Instantiate(templateObject, spawner.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject templateObject;
+    public float xMin;
+    public float xMax;
+    public int numOfBalls;
+    public float timeMin;
+    public float timeMax;
+
+
+    void Start()
+    {
+        for (int i = 0; i < numOfBalls; i++)
+        {
+            Invoke("SpawnBasketball", Random.Range(timeMin, timeMax));
+
+        }
+    }
+
+
+    void SpawnBasketball() {
+        float xRand = Random.Range(transform.position.x-xMin, transform.position.z-xMax);
+        Vector3 pos = new Vector3(xRand, 1.0f, transform.position.z);
+        Debug.Log(pos);
+        Instantiate(templateObject, pos, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    //void Update () {
+       // Instantiate(templateObject);
+    //}
+}
